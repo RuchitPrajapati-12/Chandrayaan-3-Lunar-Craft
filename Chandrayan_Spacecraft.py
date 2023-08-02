fa268c6d9b37531265943af872383489d5f796be
RuchitPrajapati-12 <ruchitprajapati1191@gmail.com>
2023-08-02 14:47:11 +0530

    Define the Spacecraft class with an empty implementation

---
+++ Chandrayan_Spacecraft.py
@@ -0,0 +1,28 @@
+class Spacecraft:
+    def __init__(self, x, y, z, direction):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.direction = direction
+
+    # Methods for movement and rotation
+    def move_forward(self):
+        pass
+
+    def move_backward(self):
+        pass
+
+    def turn_left(self):
+        pass
+
+    def turn_right(self):
+        pass
+
+    def turn_up(self):
+        pass
+
+    def turn_down(self):
+        pass
+
+    def execute_commands(self, commands):
+        pass
